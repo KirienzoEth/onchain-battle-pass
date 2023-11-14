0113e873acc6344e9fa6e46c116e0d8945b50120
KirienzoEth <kirienzo.eth@gmail.com>
2023-11-14 18:07:52 +0900

    Enable optimizer and gas report

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,7 +2,19 @@ import { HardhatUserConfig } from 'hardhat/config';
 import '@nomicfoundation/hardhat-toolbox';
 
 const config: HardhatUserConfig = {
-  solidity: '0.8.23',
+  solidity: {
+    version: '0.8.23',
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 1000,
+      },
+    },
+  },
+  gasReporter: {
+    enabled: true,
+    currency: 'USD',
+  },
 };
 
 export default config;
